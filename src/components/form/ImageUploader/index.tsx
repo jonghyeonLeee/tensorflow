5e1989505272d7e8ca29b903677bfcdf57823890
jonghyeon <jonghyeonLee.90@gmail.com>
2023-04-06 23:57:30 +0900

    tensor flow test

--- src/components/form/ImageUploader/index.tsx
+++ src/components/form/ImageUploader/index.tsx
@@ -1,9 +1,13 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
+
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 const ImageUploader = () => {
   const [imageData, setImageData] = useState<string | null>(null);
+  const [calImageData, setCalImageData] = useState<string>('');
+  const imgRef = useRef(null);
 
-  function handleFileInputChange(e: ChangeEvent<HTMLInputElement>) {
+  const handleFileInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
@@ -13,13 +17,36 @@ const ImageUploader = () => {
     };
 
     reader.readAsDataURL(file);
-  }
+  };
+
+  const handleClickTest = async () => {
+    console.log('handleClickTest');
+  };
+
+  const handelChange = async () => {
+    // console.log(imgRef);
+    // console.log(imageData);
+
+    // Load the model.
+    const model = await mobilenet.load();
+    console.log(model);
+    //
+    // // Classify the image.
+    // const predictions = await model.classify();
+    // console.log(predictions);
+  };
 
   return (
     <div>
       <input type='file' onChange={handleFileInputChange} />
       {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-      {imageData && <img src={imageData} alt='Uploaded Image' />}
+      {imageData && (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/img-redundant-alt,jsx-a11y/no-noninteractive-element-interactions
+        <img ref={imgRef} src={imageData} alt='Uploaded Image' onClick={handleClickTest} />
+      )}
+      {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+      {calImageData !== '' ? <img src={calImageData} alt='Uploaded Image' /> : null}
+      <button onClick={handelChange}>변환</button>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/tfhub': {
+        target: 'https://tfhub.dev',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/tfhub/, ''),
+      },
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
